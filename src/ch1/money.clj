f1b05e3b06ea512979738b39f80a6c05e9a2d3a2
Ryan Watkins <ryanwatkins54@gmail.com>
2018-09-19 16:36:27 +0000

    mod: update ch1 code

--- src/ch1/money.clj
+++ src/ch1/money.clj
@@ -17,5 +17,27 @@
        (ex-info "Currencies do not match."
                 {:m1 m1 :m2 m2}))))
 
+(defn =$
+  ([m1] true)
+  ([m1 m2] (zero? (.compareTo m1 m2)))
+  ([m1 m2 & monies]
+   (every? zero? (map #(.compareTo m1 %) (conj monies m2)))))
+
+(defn +$
+  ([m1] m1)
+  ([m1 m2]
+   (validate-same-currency m1 m2)
+   (-> Money (+ (:amount m1) (:amount m2)) (:currency m1)))
+  ([ m1 m2 & monies]
+   (reduce +$ m1 (conj monies m2))))
+
+
 (def currencies {:gbp (->Currency 100 "GBP" "Pound sterling")
                  :usd (->Currency 100 "USD" "US dollar")})
+
+(defn make-money
+  ([] (make-money 0))
+  ([amount] (make-money amount (:usd currencies)))
+  ([amount currency] (->Money amount currency)))
+
+(make-money)
